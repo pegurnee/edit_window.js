af2f10b44b7ddbe56a8cc25bed104936368f589c
Eddie Gurnee <pegurnee@gmail.com>
2015-07-28 14:24:03 -0400

    added color selection and closed da function

--- edit_window.js
+++ edit_window.js
@@ -1,47 +1,56 @@
-function egEdit(elem, callbackFunction) {
-  var editWindow,
-    editConfirm,
-    editText;
-
-  elem.disabled = true;
-
-  editWindow = document.createElement('div');
-
-  editWindow.id = 'egEditWindow';
-  editWindow.style.position = 'fixed';
-  editWindow.style.width = '600px';
-  editWindow.style.height = '400px';
-  editWindow.style.top = '100px';
-  editWindow.style.left = 'calc(50% - 300px)';
-  editWindow.style.backgroundColor = 'blue';
-
-  editConfirm = document.createElement('input');
-  editConfirm.style.position = 'absolute';
-  editConfirm.style.bottom = '5px';
-  editConfirm.style.right = '10px';
-  editConfirm.type = 'button';
-  editConfirm.value = 'Finish Editing';
-  editConfirm.onclick = function() {
-    elem.value = editText.value;
-    elem.disabled = false;
-    if (callbackFunction) {
-      callbackFunction(editText.value);
-    }
-    document.body.removeChild(editWindow);
+var EditWindow = (function() {
+  function EditWindow(backgroundColor, foregroundColor) {
+    this.backgroundColor = backgroundColor ? backgroundColor : 'blue';
+    this.foregroundColor = foregroundColor ? foregroundColor : 'green';
   }
 
-  editWindow.appendChild(editConfirm);
+  EditWindow.prototype.edit = function(elem, functionToApply) {
+    var editWindow,
+      editConfirm,
+      editText;
 
-  editText = document.createElement('textarea');
-  editText.style.marginLeft = '10px';
-  editText.style.marginTop = '10px';
-  editText.style.width = 'calc(100% - 25px)';
-  editText.style.height = 'calc(100% - 45px)';
-  editText.style.backgroundColor = 'green';
-  editText.style.fontSize = '18px';
-  editText.value = elem.value;
+    elem.disabled = true;
 
-  editWindow.appendChild(editText);
+    editWindow = document.createElement('div');
 
-  document.body.appendChild(editWindow);
-}
+    editWindow.id = 'egEditWindow';
+    editWindow.style.position = 'fixed';
+    editWindow.style.width = '600px';
+    editWindow.style.height = '400px';
+    editWindow.style.top = '100px';
+    editWindow.style.left = 'calc(50% - 300px)';
+    editWindow.style.backgroundColor = this.backgroundColor;
+
+    editConfirm = document.createElement('input');
+    editConfirm.style.position = 'absolute';
+    editConfirm.style.bottom = '5px';
+    editConfirm.style.right = '10px';
+    editConfirm.type = 'button';
+    editConfirm.value = 'Finish Editing';
+    editConfirm.onclick = function() {
+      elem.value = editText.value;
+      elem.disabled = false;
+      if (functionToApply) {
+        functionToApply(editText.value);
+      }
+      document.body.removeChild(editWindow);
+    }
+
+    editWindow.appendChild(editConfirm);
+
+    editText = document.createElement('textarea');
+    editText.style.marginLeft = '10px';
+    editText.style.marginTop = '10px';
+    editText.style.width = 'calc(100% - 25px)';
+    editText.style.height = 'calc(100% - 45px)';
+    editText.style.backgroundColor = this.foregroundColor;
+    editText.style.fontSize = '18px';
+    editText.value = elem.value;
+
+    editWindow.appendChild(editText);
+
+    document.body.appendChild(editWindow);
+  }
+
+  return EditWindow;
+})();
